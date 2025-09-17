c2f8383f1a9a561db9954d4c4496596040a1b338
mrstrange1708 <junaidsamishaik@gmail.com>
2025-09-18 00:23:14 +0530

    added the login

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import { BackgroundBeams } from "../../components/ui/background-beams";
+import SignupFormDemo from "../../components/signup-form-demo"
+
+export default function login() {
+  return (
+    <div className="h-[100vh] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+        <SignupFormDemo />
+      <BackgroundBeams />
+    </div>
+  );
+}
+
